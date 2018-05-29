1e4ef0371aa19a05bc54145a74dc32806f528004
Harry Doddema <harrydoddema@promocat.nl>
2018-05-29 16:41:56 +0200

    default = hover

--- widgets/grid/GridView.php
+++ widgets/grid/GridView.php
@@ -34,7 +34,7 @@ class GridView extends \yii\grid\GridView
     /**
      * @var bool is striped
      */
-    public $striped = false;
+    public $striped = true;
 
     /**
      * @var bool is row have hover effect
